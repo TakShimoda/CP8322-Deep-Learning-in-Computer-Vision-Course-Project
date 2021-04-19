d45d53569750e5c678c9e7a3d81b564ad5e953f6
TakShimoda <gshimoda@ryerson.ca>
2021-04-19 13:54:08 -0400

    Update resnet_eval.py
    Updated evaluation code on baseline resnet

--- classification/main/resnet_eval.py
+++ classification/main/resnet_eval.py
@@ -3,10 +3,10 @@ import torchvision
 from torchvision import transforms
 from PIL import Image
 from dataloader import *
+from eval import AverageMeter, accuracy
 
 #Global variables
-CUDA_AVAILABLE = torch.cuda.is_available()
-if CUDA_AVAILABLE:
+if torch.cuda.is_available():
     print('===========USING CUDA===========')
     device = 'cuda'
 else:
@@ -21,47 +21,9 @@ top_1_accuracy = 0
 top_5_accuracy = 0
 batch_size = 32
 
-class AverageMeter(object):
-    """Computes and stores the average and current value
-       Imported from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L247-L262
-    """
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-def accuracy(output, target, topk):
-    """ Returns the top-1 and top-5 accuracy on the batch
-    Args:
-        output(torch.tensor): the raw output of the model on the batch
-        target(torch.tensor): the target labels on the batch
-        topk(tuple): a tuple representing the topk to use: is (1, 5)
-    Returns:
-        res(list): a list of torch tensors for top1 and top5 accuracy
-    """
-    probabilities = torch.nn.functional.softmax(output)
-    _, pred = probabilities.data.topk(5, 1)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-    res = []
-    for k in topk:
-        correct_k = correct[:k].contiguous().view(-1).float().sum(0)
-        res.append(correct_k.mul_(100.0 / batch_size))
-    return res
-
 if __name__ == '__main__':
     top1, top5 = AverageMeter(), AverageMeter()
-    val_loader = load_data(model_name, device, dir)
+    val_loader = load_data(model_name, device, dir, batch_size)
     for index, (img, target) in enumerate(val_loader):
         img, target = img.cuda(non_blocking=True), target.cuda(non_blocking=True)
         with torch.no_grad():
